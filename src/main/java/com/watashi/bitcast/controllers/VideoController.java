2bb74243425549a720728654973e881519f3e616
watashi-00 <kalitajj374@gmail.com>
2026-08-16 18:14:47 -0300

    feat: add index.html with video upload and playback functionality and enable CORS in VideoController

--- src/main/java/com/watashi/bitcast/controllers/VideoController.java
+++ src/main/java/com/watashi/bitcast/controllers/VideoController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import com.watashi.bitcast.dto.VideoInfo;
 import com.watashi.bitcast.services.VideoService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/videos")
 public class VideoController {
     
